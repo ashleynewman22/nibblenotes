ada408716716afe53a583dcc7ce095f2ab0e4bb9
bernardmanikwa <bernard.manikwa@outlook.com>
2024-09-10 12:29:53 +0200

    validations and associations

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,2 +1,5 @@
 class Note < ApplicationRecord
+  belongs_to  :recipe
+
+  validates :content, presence: true
 end
---
+++ app/models/recipe_category.rb
@@ -0,0 +1,6 @@
+class RecipeCategory < ApplicationRecord
+  belongs_to :recipe
+  belongs_to :category
+
+  validates :recipe_id, uniqueness
+end
