69044c02e7b7cd8b352ad2387603ac01390e5ea9
Ashley Newman <ashleynew2122002@gmail.com>
2024-09-11 11:53:11 +0200

    Added controller and views for recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,38 @@
 class RecipesController < ApplicationController
+  def index
+    @recipes = Recipe.where(user: current_user)
+    @categories = Category.all
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def new
+    @recipe = Recipe.new # need to use simple_form_for for user to create new item rental
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.user = current_user
+    if @recipe.save
+      redirect_to @recipe
+      # redirect_to recipe_path(@recipe)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+
+    @recipe.destroy
+    redirect_to recipes_path, status: :see_other
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :ingredients, :description, category_ids: [])
+  end
 end
